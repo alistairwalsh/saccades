f6fb003e642bca77ff582a49dfe0d9eec5c93cf3
alistairwalsh <alistair.walsh@me.com>
2014-09-14 09:57:52 +1000

    better loop in calculate minute averages

--- calculate_minute_averages.R
+++ calculate_minute_averages.R
@@ -5,10 +5,13 @@ The mean values for each minute are then put in the vector 'gather'
 for later use i.e. plotting
 
 '''
+
+
+
 gather = NULL
 for (i in as.integer((levels(dfile$Minute)))){
-  current = mean(dfile$Average.Speed[dfile$Minute == i])
-  gather = append(gather,current)
+  gather[i] = mean(dfile$Average.Speed[dfile$Minute == i])
+  
 }
 
 plot(gather)
