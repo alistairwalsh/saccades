a7a6c6a7a173f2704914745d554bc2450c51a956
alistairwalsh <alistair.walsh@me.com>
2014-09-16 19:17:54 +1000

    again

---
+++ fix_broken_files.R
@@ -0,0 +1,16 @@
+fhandle1 <- saccade_data_file_list[1]
+fhandle2 <- saccade_data_file_list[2]
+
+newname <- strsplit(saccade_data_file_list[2], " part2")
+
+dfile_part1 <- read.csv(fhandle1,header=TRUE,sep='\t',skip = 9)
+dfile_part2 <- read.csv(fhandle2,header=TRUE,sep='\t',skip = 9)
+
+tail(dfile_part1[1:6])
+head(dfile_part2[1:6])
+
+newDfile <- subset.data.frame(dfile, select= c(6,11,12,14))
+
+
+
+13090691482-12415905404
