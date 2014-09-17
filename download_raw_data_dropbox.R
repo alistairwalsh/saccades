58818565e7c0f1a1b10aadebfaafed0f8460e9e2
alistairwalsh <alistair.walsh@me.com>
2014-09-17 05:21:15 +0000

    fixed for linux

--- download_raw_data_dropbox.R
+++ download_raw_data_dropbox.R
@@ -1,6 +1,12 @@
-link = 'https://www.dropbox.com/s/64y50x80njhpq4b/raw%20data.zip?dl=0'
+
+if (!file.exists('~/saccades/data')){
+  dir.create('~/saccades/data')
+}
+
+link = 'https://www.dropbox.com/s/e5wqj9mqrcu3vjz/Archive.zip?dl=0'
 # method = wget is appropriate for Linux, curl might be better for OSX
-download.file(link,'data.zip', method = 'curl')
+download.file(link, destfile = '~/saccades/data/Archive.zip', method = 'wget')
 
 # change list to FALSE to actually unzip files
-unzip('data.zip',list = TRUE)
+unzip('~/saccades/data/Archive.zip',exdir = "~/saccades/data/", list = FALSE)
+
