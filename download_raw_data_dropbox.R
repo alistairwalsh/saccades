f4e869a39fb029677203e55fe080c011f35c08fc
alistairwalsh <alistair.walsh@me.com>
2014-09-17 13:54:00 +1000

    script for data download

---
+++ download_raw_data_dropbox.R
@@ -0,0 +1,8 @@
+
+# method = wget is appropriate for Linux, curl might be better for OSX
+download.file('https://www.dropbox.com/s/64y50x80njhpq4b/raw%20data.zip?dl=1'
+              ,'data.zip'
+              , method = 'wget')
+
+# change list to TRUE to actually unzip files
+unzip('data.zip',list = TRUE)
