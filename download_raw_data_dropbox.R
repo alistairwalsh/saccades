6dc39459a2dc4a6381e9e20b2e2d7fe9e18db881
alistairwalsh <alistair.walsh@me.com>
2014-09-17 14:29:11 +1000

    automated git setup. Trying to find working download method

--- download_raw_data_dropbox.R
+++ download_raw_data_dropbox.R
@@ -1,8 +1,6 @@
-
+link = 'https://www.dropbox.com/s/64y50x80njhpq4b/raw%20data.zip?dl=0'
 # method = wget is appropriate for Linux, curl might be better for OSX
-download.file('https://www.dropbox.com/s/64y50x80njhpq4b/raw%20data.zip?dl=1'
-              ,'data.zip'
-              , method = 'wget')
+download.file(link,'data.zip', method = 'curl')
 
 # change list to FALSE to actually unzip files
 unzip('data.zip',list = TRUE)
---
+++ git_configure.sh
@@ -0,0 +1,6 @@
+
+
+echo 'Setting github credentials for Alistair'
+
+git config --global user.email "alistair.walsh@me.com"
+git config --global user.name "alistairwalsh"
