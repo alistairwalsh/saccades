d1a09f9c1ac15b35798a0cb6b5581efe18017ca7
alistairwalsh <alistair.walsh@me.com>
2014-09-17 21:28:14 +1000

    download OSX doesn't work

--- download_raw_data_dropbox_OSX.R
+++ download_raw_data_dropbox_OSX.R
@@ -3,9 +3,12 @@ if (!file.exists('./data')){
   dir.create('./data')
 }
 
-link = 'https://www.dropbox.com/s/e5wqj9mqrcu3vjz/Archive.zip?dl=1'
+#
+# link = 'https://www.dropbox.com/s/e5wqj9mqrcu3vjz/Archive.zip?dl=1'
 # method = wget is appropriate for Linux, curl might be better for OSX
-download.file(link, destfile = './data/Archive.zip',method = 'curl')
+# download.file(link, destfile = './data/Archive.zip',method = 'curl')
+### Gave up and manually downloaded the .zip ### 
+
 
 # change list to FALSE to actually unzip files
 unzip('./data/Archive.zip',exdir = "~/Documents/Rprojects/saccades/data/", list = FALSE)
